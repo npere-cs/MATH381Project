c06cf45618d8db2551b4705b98229572522634b9
dev <71530444+npere-cs@users.noreply.github.com>
2022-05-15 16:33:13 -0700

    changes to code

--- heuristic.py
+++ heuristic.py
@@ -1,19 +1,10 @@
 import numpy as np
 # from pulp import LpMinimize, LpProblem, LpStatus, lpSum, LpVariable
 import pulp as p
-
-# MS LP
+import parsing
 
 model = p.LpProblem(name="Heuristic", sense=p.LpMinimize)
-'''
-------------------------------------------
-For use later when we want to figure out the number of people to assign
-to each location
-Dictionary with transactional data:
-Key: value
-weekday: df of (rows: timeslots, cols: locations)
-------------------------------------------
-'''
+
 # x = []
 # for i in range(0, 24):
 #   if (i < 10):
@@ -46,22 +37,31 @@ weekday: df of (rows: timeslots, cols: locations)
 #   print(f"{name}: {constraint.value()}")
 
 # represents number of FIXED classified positions at location
-num_classified = 1
+num_classified = {
+  "ms": 0,
+  "mg": 0,
+  "ps": 0,
+  "eg": 0,
+  "bg": 0,
+  "op": 0,
+  "hg": 0,
+  "ov": 0
+}
 
-# represents the number of workers available to us on each day of week
-num_workers = [8, 7, 7, 8, 7]
+# represents the number of workers available to us on each day of week at each location
+num_workers = {
+  "ms": [1, 2, 3, 4, 5, 6, 7],
+  "mg": [1, 2, 3, 4, 5, 6, 7],
+  "ps": [1, 2, 3, 4, 5, 6, 7],
+  "eg": [1, 2, 3, 4, 5, 6, 7],
+  "bg": [1, 2, 3, 4, 5, 6, 7],
+  "hg": [1, 2, 3, 4, 5, 6, 7],
+  "ov": [1, 2, 3, 4, 5, 6, 7]
+}
 
-dec_var = []
-'''
-Times in which we can have employees start a shift in
-7:00 7:30 8:00 ... 16:30 17:00
+# Represents the abbreviated location codes
+LOCATIONS = ["ms", "mg", "ps", "eg", "bg", "op", "hg", "ov"]
 
-Shift lengths:
-2 hrs, 2:30 hrs, 3 hrs, 3:30 hrs, 4 hrs, 4:30 hrs, 5 hrs
-Classified (FIXED):
-8:30 hrs
-
-'''
 # Represent the time intervals at which individual 
 BUCKETS = [
   "06:30", "07:00", "07:30", "08:00", "08:30",
@@ -70,22 +70,30 @@ BUCKETS = [
   "14:00",  "14:30", "15:00", "15:30", "16:00",
   "16:30", "17:00", "17:30", "18:00", "18:30"
 ]
+
+'''
+Shift lengths:
+2 hrs, 2:30 hrs, 3 hrs, 3:30 hrs, 4 hrs, 4:30 hrs, 5 hrs
+Classified (FIXED):
+8:30 hrs
+'''
+# Represents the possible shift lengths employees can take on
 SHIFTS = ["2:00", "2:30", "3:00", "3:30", "4:00", "4:30", "5:00"]
 
-# Fills the decision variables for each time bucket for regular employees
-for start_time in range(len(BUCKETS)):
-  shift_times = []
-  # Fills the shift lengths at the given time bucket
-  for shift in range(len(SHIFTS)):
-    shift_times.append(p.LpVariable(\
-      name=\
-      "People starting at " + BUCKETS[start_time] + "with " + SHIFTS[shift] + " long shift", \
-      lowBound=0, cat="Integer"))
-  # Fills decision variables for classified workers (8.5 hr fixed shift)
-  shift_times.append(p.LpVariable(\
-    name = "Classified starting at " + BUCKETS[start_time] + "with 8:30 long shift",\
-    lowBound=0, cat="Integer"))
-  dec_var.append(shift_times)
+# # Fills the decision variables for each time bucket for regular employees
+# for start_time in range(len(BUCKETS)):
+#   shift_times = []
+#   # Fills the shift lengths at the given time bucket
+#   for shift in range(len(SHIFTS)):
+#     shift_times.append(p.LpVariable(\
+#       name=\
+#       "People starting at " + BUCKETS[start_time] + "with " + SHIFTS[shift] + " long shift", \
+#       lowBound=0, cat="Integer"))
+#   # Fills decision variables for classified workers (8.5 hr fixed shift)
+#   shift_times.append(p.LpVariable(\
+#     name = "Classified starting at " + BUCKETS[start_time] + "with 8:30 long shift",\
+#     lowBound=0, cat="Integer"))
+#   dec_var.append(shift_times)
 
 
 
@@ -99,4 +107,27 @@ key: value
 weekday: df of (rows: ALL time buckets, cols: locations)
 
 
+days of the week - outer
+location - middle
+buckets - deepest
+
 '''
+
+dec_var = []
+
+# dictionary containing the transactional data for each day of the week at each location
+# for each 30 minute interval
+transacs = parsing.parsedHalfHourData()
+# days of the week
+weekdays = list(transacs.keys())
+# locations
+locations = list(transacs[weekdays[0]].columns[1:9])
+
+
+# # loop thru the days
+# for day in weekdays:
+#   locations = []
+  
+#   headers = df.columns
+#   # loop thru the locations
+#   for 
