f92d7759031bdecefcd53ae9f73180016ed3379b
Jonny Kwan <jon.s.kwan@gmail.com>
2022-05-16 23:49:39 -0700

    All histograms made

--- histograms.py
+++ histograms.py
@@ -3,13 +3,17 @@ import pandas as pd
 import parsing
 import matplotlib.pyplot as plt
 
-trans_data = parsing.parsedHalfHourData()
-mon_trans_data = trans_data["Mon"]
 locations = ["BG", "EG", "HG", "MG", "MS", "OP", "OV", "PS"]
-for loc in locations:
-  ax = mon_trans_data.plot.bar(x="Timeslot", y=loc)
-  fig = ax.get_figure()
-  fig.savefig("graphs/Monday at " + loc + ".png")
+weekdays = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]
+trans_data = parsing.parsedHalfHourData()
+for day in weekdays:
+  for loc in locations:
+    ax = trans_data[day[0:3]].plot.bar(x="Timeslot", y=loc)
+    fig = ax.get_figure()
+    fig.savefig("graphs/" + day + " at " + loc + ".png")
 ax = trans_data["Sat"].plot.bar(x="Timeslot", y="HG")
 fig = ax.get_figure()
 fig.savefig("graphs/Saturday at HG.png")
+ax = trans_data["Sun"].plot.bar(x="Timeslot", y="HG")
+fig = ax.get_figure()
+fig.savefig("graphs/Sunday at HG.png")
