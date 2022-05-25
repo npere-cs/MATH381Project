54b9151fad470416a0c1fbe9f166832ab6443ce0
Jonny Kwan <jon.s.kwan@gmail.com>
2022-05-24 22:51:06 -0700

    New bar charts for totals

--- histograms.py
+++ histograms.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 
 locations = ["BG", "EG", "HG", "MG", "MS", "OP", "OV", "PS"]
 weekdays = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]
+"""
 trans_data = parsing.parsedHalfHourData()
 for day in weekdays:
   for loc in locations:
@@ -17,3 +18,11 @@ fig.savefig("graphs/Saturday at HG.png")
 ax = trans_data["Sun"].plot.bar(x="Timeslot", y="HG")
 fig = ax.get_figure()
 fig.savefig("graphs/Sunday at HG.png")
+"""
+daily_averages = parsing.parsedTotals()
+daily_averages = daily_averages.loc[:, daily_averages.columns!="Weekday"].transpose()
+for i in range(len(weekdays)):
+  ax = daily_averages.plot.bar(y=i)
+  ax.get_legend().remove()
+  fig = ax.get_figure()
+  fig.savefig("graphs/" + weekdays[i] + " Totals.png")
